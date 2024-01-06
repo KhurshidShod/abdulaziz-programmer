a68067bea7f1cc27e4739408a64acf5e82622bc7
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2024-01-07 00:01:18 +0500

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,94 @@
+"use client";
+import { useRef, useState } from "react";
+import Magnetic from "@/commons/Magnetic";
+import styles from "./style.module.scss";
+import { BsMoonFill } from "react-icons/bs";
+import { FaSun } from "react-icons/fa";
+
+const Header = () => {
+    const [themeDark, setThemeDark] = useState(false);
+  const sliderRef = useRef(null);
+
+  const sliderMouseOver = (e) => {
+    const left = e.currentTarget.offsetLeft;
+    const width = e.currentTarget.getBoundingClientRect().width;
+    sliderRef.current.style.left = `${left + width / 2}px`;
+    sliderRef.current.style.width = `${width}px`;
+  };
+  const sliderMouseLeave = (e) => {
+    sliderRef.current.style.left = `15px`;
+    sliderRef.current.style.width = `50px`;
+    sliderRef.current.style.height = `3px`;
+    sliderRef.current.style.borderRadius = `15px`;
+  };
+  return (
+    <header className={styles.header}>
+      <div className="container">
+        <nav className={styles.nav}>
+          <div ref={sliderRef} className={styles.indicator}></div>
+          <div className={styles.nav__logo}>
+            <h1>AP</h1>
+          </div>
+          <ul>
+            <li>
+              <Magnetic>
+                <li
+                  onMouseEnter={(e) => sliderMouseOver(e)}
+                  onMouseLeave={(e) => sliderMouseLeave(e)}
+                >
+                  About Me
+                </li>
+              </Magnetic>
+              <Magnetic>
+                <li
+                  onMouseEnter={(e) => sliderMouseOver(e)}
+                  onMouseLeave={(e) => sliderMouseLeave(e)}
+                >
+                  Skills
+                </li>
+              </Magnetic>
+              <Magnetic>
+                <li
+                  onMouseEnter={(e) => sliderMouseOver(e)}
+                  onMouseLeave={(e) => sliderMouseLeave(e)}
+                >
+                  Projects
+                </li>
+              </Magnetic>
+              <Magnetic>
+                <li
+                  onMouseEnter={(e) => sliderMouseOver(e)}
+                  onMouseLeave={(e) => sliderMouseLeave(e)}
+                >
+                  Contact Me
+                </li>
+              </Magnetic>
+            </li>
+          </ul>
+          <div className={styles.nav__actions}>
+          <Magnetic>
+            <button onMouseEnter={(e) => {
+                const left = e.currentTarget.offsetLeft;
+                const width = e.currentTarget.getBoundingClientRect().width;
+                sliderRef.current.style.left = `${left + width / 2}px`;
+                sliderRef.current.style.width = `5px`;
+                sliderRef.current.style.height = `5px`;
+                sliderRef.current.style.borderRadius = `50%`;
+            }} onMouseLeave={(e) => {
+                sliderRef.current.style.left = `15px`;
+                sliderRef.current.style.width = `50px`;
+                sliderRef.current.style.height = `3px`;
+                sliderRef.current.style.borderRadius = `15px`;
+              }} onClick={() => {
+                document.body.classList.toggle("dark");
+                setThemeDark(!themeDark)
+            }}>{themeDark ? <FaSun size={20} /> : <BsMoonFill size={20} />}</button>
+            </Magnetic>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,182 @@
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import styles from "./style.module.scss";
+import Magnetic from "@/commons/Magnetic";
+import Image from "next/image";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTelegram, FaYoutube, FaLinkedin } from "react-icons/fa";
+
+const Hero = () => {
+  const heroSvg1 = useRef(null);
+  const heroSvg2 = useRef(null);
+  const heroSvg3 = useRef(null);
+  const heroSvg4 = useRef(null);
+  const heroSvg5 = useRef(null);
+  const heroSvg6 = useRef(null);
+  const heroSvg7 = useRef(null);
+  const heroSvg8 = useRef(null);
+  const heroSvg9 = useRef(null);
+  const animateSvgs = (e) => {
+    const speed1 = heroSvg1.current.getAttribute("data-speed");
+    const speed2 = heroSvg2.current.getAttribute("data-speed");
+    const speed3 = heroSvg3.current.getAttribute("data-speed");
+    const speed4 = heroSvg4.current.getAttribute("data-speed");
+    const speed5 = heroSvg5.current.getAttribute("data-speed");
+    const speed6 = heroSvg6.current.getAttribute("data-speed");
+    const speed7 = heroSvg7.current.getAttribute("data-speed");
+    const speed8 = heroSvg8.current.getAttribute("data-speed");
+    const speed9 = heroSvg9.current.getAttribute("data-speed");
+    const x1 = (window.innerWidth - e.clientX * speed1) / 150;
+    const y1 = (window.innerHeight - e.clientY * speed1) / 150;
+    const x2 = (window.innerWidth - e.clientX * speed2) / 150;
+    const y2 = (window.innerHeight - e.clientY * speed2) / 150;
+    const x3 = (window.innerWidth - e.clientX * speed3) / 150;
+    const y3 = (window.innerHeight - e.clientY * speed3) / 150;
+    const x4 = (window.innerWidth - e.clientX * speed4) / 150;
+    const y4 = (window.innerHeight - e.clientY * speed4) / 150;
+    const x5 = (window.innerWidth - e.clientX * speed5) / 150;
+    const y5 = (window.innerHeight - e.clientY * speed5) / 150;
+    const x6 = (window.innerWidth - e.clientX * speed6) / 150;
+    const y6 = (window.innerHeight - e.clientY * speed6) / 150;
+    const x7 = (window.innerWidth - e.clientX * speed7) / 150;
+    const y7 = (window.innerHeight - e.clientY * speed7) / 150;
+    const x8 = (window.innerWidth - e.clientX * speed8) / 150;
+    const y8 = (window.innerHeight - e.clientY * speed8) / 150;
+    const x9 = (window.innerWidth - e.clientX * speed9) / 150;
+    const y9 = (window.innerHeight - e.clientY * speed9) / 150;
+    heroSvg1.current.style.transform = `translateX(${x1}px) translateY(${y1}px)`;
+    heroSvg2.current.style.transform = `translateX(${x2}px) translateY(${y2}px)`;
+    heroSvg3.current.style.transform = `translateX(${x3}px) translateY(${y3}px)`;
+    heroSvg4.current.style.transform = `translateX(${x4}px) translateY(${y4}px)`;
+    heroSvg5.current.style.transform = `translateX(${x5}px) translateY(${y5}px)`;
+    heroSvg6.current.style.transform = `translateX(${x6}px) translateY(${y6}px)`;
+    heroSvg7.current.style.transform = `translateX(${x7}px) translateY(${y7}px)`;
+    heroSvg8.current.style.transform = `translateX(${x8}px) translateY(${y8}px)`;
+    heroSvg9.current.style.transform = `translateX(${x9}px) translateY(${y9}px)`;
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => animateSvgs(e));
+  }, []);
+  return (
+    <section className={styles.hero}>
+      <div className="container">
+        <div className={styles.hero__wrapper}>
+          <img
+            ref={heroSvg1}
+            data-speed="-7"
+            src="/images/heroSvgs/1.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg2}
+            data-speed="3"
+            src="/images/heroSvgs/2.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg3}
+            data-speed="5"
+            src="/images/heroSvgs/3.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg4}
+            data-speed="-1"
+            src="/images/heroSvgs/4.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg5}
+            data-speed="4"
+            src="/images/heroSvgs/5.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg6}
+            data-speed="-8"
+            src="/images/heroSvgs/6.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg7}
+            data-speed="6"
+            src="/images/heroSvgs/7.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg8}
+            data-speed="-2"
+            src="/images/heroSvgs/8.svg"
+            className={styles.herosvg}
+          />
+          <img
+            ref={heroSvg9}
+            data-speed="4"
+            src="/images/heroSvgs/9.svg"
+            className={styles.herosvg}
+          />
+          <div className={styles.hero__wrapper_text}>
+            <h1>
+              Hello I'am <span>Abdulaziz Toshpo'latov. Frontend</span>{" "}
+              <span>Developer</span> based in <span>Tashkent</span>
+            </h1>
+            <p>
+              I'm Evren Shah Lorem Ipsum is simply dummy text of the printing
+              and typesetting industry. Lorem Ipsum has been the industry's
+              standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to specimen book.
+            </p>
+            <div className={styles.hero__wrapper_text_socials}>
+              <Magnetic>
+                <button className={styles.hero__wrapper_text_socials_social}>
+                  <Magnetic>
+                    <span>
+                      <AiFillInstagram />
+                    </span>
+                  </Magnetic>
+                </button>
+              </Magnetic>
+              <Magnetic>
+                <button className={styles.hero__wrapper_text_socials_social}>
+                  <Magnetic>
+                    <span>
+                      <FaTelegram />
+                    </span>
+                  </Magnetic>
+                </button>
+              </Magnetic>
+              <Magnetic>
+                <button className={styles.hero__wrapper_text_socials_social}>
+                  <Magnetic>
+                    <span>
+                      <FaYoutube />
+                    </span>
+                  </Magnetic>
+                </button>
+              </Magnetic>
+              <Magnetic>
+                <button className={styles.hero__wrapper_text_socials_social}>
+                  <Magnetic>
+                    <span>
+                      <FaLinkedin />
+                    </span>
+                  </Magnetic>
+                </button>
+              </Magnetic>
+            </div>
+          </div>
+          <div className={styles.hero__wrapper_image}>
+            <Image
+              src="/images/hero.svg"
+              alt="A guy"
+              width={700}
+              height={600}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
