6c594a4af1432c8409a32cfa38751d9255e2e952
assavlatbek <assavlatbek@gmail.com>
2024-01-23 13:02:58 +0500

    code

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -31,7 +31,7 @@ const Projects = () => {
     },
     {
       index: "04",
-      image: "/images/projects/1.jpg",
+      image: "/images/projects/2.jpg",
       title: "Crypto Screener Application",
       descr:
         "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.",
