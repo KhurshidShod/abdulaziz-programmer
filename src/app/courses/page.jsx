86600a7ef1a31d516493757ee03b32c04fe6022e
assavlatbek <assavlatbek@gmail.com>
2024-01-28 04:00:31 +0500

    |!

---
+++ src/app/courses/page.jsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <div className="content">
+        <h1>Coming soon</h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
