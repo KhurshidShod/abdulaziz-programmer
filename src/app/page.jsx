d653534cb76cfb7301da6d0aad849689fb3fee91
assavlatbek <assavlatbek@gmail.com>
2024-01-28 02:09:35 +0500

    code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Experiences from "@/components/Experiences";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
-import Testimonials from "@/components/Testimonials";
+import Testimonials from "@/components/Students";
 import Statistics from "@/components/Statistics";
 import Contact from "@/components/Contact";
 import Projects from "@/components/Projects";
