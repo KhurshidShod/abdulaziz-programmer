647e4d4c8465fd0bed59e3ad1605278f9d39d5fc
assavlatbek <assavlatbek@gmail.com>
2024-01-27 17:03:18 +0500

    co

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,7 +5,7 @@ import Magnetic from "@/commons/Magnetic";
 import Image from "next/image";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaTelegram, FaYoutube, FaLinkedin } from "react-icons/fa";
-import { Element } from 'react-scroll';
+import { Element } from "react-scroll";
 
 const Hero = () => {
   const path = useRef(null);
@@ -16,50 +16,53 @@ const Hero = () => {
 
   useEffect(() => {
     setPath(progress);
-  }, [])
+  }, []);
   const setPath = (progress) => {
     const width = window.innerWidth;
-    path.current.setAttributeNS(null, "d", `M0 250 Q${width * x} ${250 + progress}, ${width} 250`)
-  }
+    path.current.setAttributeNS(
+      null,
+      "d",
+      `M0 250 Q${width * x} ${250 + progress}, ${width} 250`
+    );
+  };
 
-  const lerp = (x, y, a) => x * (1 - a) + y * a
+  const lerp = (x, y, a) => x * (1 - a) + y * a;
 
   const manageMouseEnter = () => {
-    if(reqId){
-      cancelAnimationFrame(reqId)
-      resetAnimation()
+    if (reqId) {
+      cancelAnimationFrame(reqId);
+      resetAnimation();
     }
-  }
+  };
 
   const manageMouseMove = (e) => {
     const { movementY, clientX } = e;
-    const pathBound =  path.current.getBoundingClientRect();
+    const pathBound = path.current.getBoundingClientRect();
     x = (clientX - pathBound.left) / pathBound.width;
-    progress+= movementY
+    progress += movementY;
     setPath(progress);
-  }
+  };
 
   const manageMouseLeave = () => {
     animateOut();
-  }
+  };
 
   const animateOut = () => {
     const newProgress = progress * Math.sin(time);
     progress = lerp(progress, 0, 0.025);
-    time+=0.2;
+    time += 0.2;
     setPath(newProgress);
-    if(Math.abs(progress) > 0.75){
+    if (Math.abs(progress) > 0.75) {
       reqId = requestAnimationFrame(animateOut);
-    }
-    else{
+    } else {
       resetAnimation();
     }
-  }
+  };
 
   const resetAnimation = () => {
     time = Math.PI / 2;
     progress = 0;
-  }
+  };
   const heroSvg1 = useRef(null);
   const heroSvg2 = useRef(null);
   const heroSvg3 = useRef(null);
@@ -113,129 +116,128 @@ const Hero = () => {
   return (
     <Element name="hero">
       <section className={styles.hero}>
-      <div className="container">
-        <div className={styles.hero__wrapper}>
-          <img
-            ref={heroSvg1}
-            data-speed="-7"
-            src="/images/heroSvgs/1.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg2}
-            data-speed="3"
-            src="/images/heroSvgs/2.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg3}
-            data-speed="5"
-            src="/images/heroSvgs/3.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg4}
-            data-speed="-1"
-            src="/images/heroSvgs/4.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg5}
-            data-speed="4"
-            src="/images/heroSvgs/5.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg6}
-            data-speed="-8"
-            src="/images/heroSvgs/6.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg7}
-            data-speed="6"
-            src="/images/heroSvgs/7.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg8}
-            data-speed="-2"
-            src="/images/heroSvgs/8.svg"
-            className={styles.herosvg}
-          />
-          <img
-            ref={heroSvg9}
-            data-speed="4"
-            src="/images/heroSvgs/9.svg"
-            className={styles.herosvg}
-          />
-          <div className={styles.hero__wrapper_text} data-scroll data-scroll-speed="0.3">
-            <h1>
-              {`Hello I'am`} <span>{`Abdulaziz Toshpo'latov. Full-Stack`}</span>{" "}
-              <span>Developer</span> based in <span>Tashkent</span>
-            </h1>
-            <p>
-              {`I'm Evren Shah Lorem Ipsum is simply dummy text of the printing
+        <div className="container">
+          <div className={styles.hero__wrapper}>
+            <img
+              ref={heroSvg1}
+              data-speed="-7"
+              src="/images/heroSvgs/1.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg2}
+              data-speed="3"
+              src="/images/heroSvgs/2.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg3}
+              data-speed="5"
+              src="/images/heroSvgs/3.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg4}
+              data-speed="-1"
+              src="/images/heroSvgs/4.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg5}
+              data-speed="4"
+              src="/images/heroSvgs/5.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg6}
+              data-speed="-8"
+              src="/images/heroSvgs/6.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg7}
+              data-speed="6"
+              src="/images/heroSvgs/7.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg8}
+              data-speed="-2"
+              src="/images/heroSvgs/8.svg"
+              className={styles.herosvg}
+            />
+            <img
+              ref={heroSvg9}
+              data-speed="4"
+              src="/images/heroSvgs/9.svg"
+              className={styles.herosvg}
+            />
+            <div
+              className={styles.hero__wrapper_text}
+              data-scroll
+              data-scroll-speed="0.3"
+            >
+              <h1>
+                {`Hello I'am`}{" "}
+                <span>{`Abdulaziz Toshpo'latov. Full-Stack`}</span>{" "}
+                <span>Developer</span> based in <span>Tashkent</span>
+              </h1>
+              <p>
+                {`I'm Evren Shah Lorem Ipsum is simply dummy text of the printing
               and typesetting industry. Lorem Ipsum has been the industry's
               standard dummy text ever since the 1500s, when an unknown printer
               took a galley of type and scrambled it to specimen book.`}
-            </p>
-            <div className={styles.hero__wrapper_text_socials}>
-              <Magnetic>
-                <button className={styles.hero__wrapper_text_socials_social}>
-                  <Magnetic>
-                    <span>
-                      <AiFillInstagram />
-                    </span>
-                  </Magnetic>
-                </button>
-              </Magnetic>
-              <Magnetic>
-                <button className={styles.hero__wrapper_text_socials_social}>
-                  <Magnetic>
-                    <span>
-                      <FaTelegram />
-                    </span>
-                  </Magnetic>
-                </button>
-              </Magnetic>
-              <Magnetic>
-                <button className={styles.hero__wrapper_text_socials_social}>
-                  <Magnetic>
-                    <span>
-                      <FaYoutube />
-                    </span>
-                  </Magnetic>
-                </button>
-              </Magnetic>
-              <Magnetic>
-                <button className={styles.hero__wrapper_text_socials_social}>
-                  <Magnetic>
-                    <span>
-                      <FaLinkedin />
-                    </span>
-                  </Magnetic>
-                </button>
-              </Magnetic>
+              </p>
+              <div className={styles.hero__wrapper_text_socials}>
+                <Magnetic>
+                  <button className={styles.hero__wrapper_text_socials_social}>
+                    <Magnetic>
+                      <span>
+                        <AiFillInstagram />
+                      </span>
+                    </Magnetic>
+                  </button>
+                </Magnetic>
+                <Magnetic>
+                  <button className={styles.hero__wrapper_text_socials_social}>
+                    <Magnetic>
+                      <span>
+                        <FaTelegram />
+                      </span>
+                    </Magnetic>
+                  </button>
+                </Magnetic>
+                <Magnetic>
+                  <button className={styles.hero__wrapper_text_socials_social}>
+                    <Magnetic>
+                      <span>
+                        <FaYoutube />
+                      </span>
+                    </Magnetic>
+                  </button>
+                </Magnetic>
+                <Magnetic>
+                  <button className={styles.hero__wrapper_text_socials_social}>
+                    <Magnetic>
+                      <span>
+                        <FaLinkedin />
+                      </span>
+                    </Magnetic>
+                  </button>
+                </Magnetic>
+              </div>
             </div>
-          </div>
-          <div className={styles.hero__wrapper_image}>
-            <Image
-              src="/images/ap.png"
-              alt="A guy"
-              width={700}
-              height={600}
-            />
+            <div className={styles.hero__wrapper_image}>
+              <Image
+                src="/images/ap.png"
+                alt="A guy"
+                width={700}
+                height={600}
+              />
+            </div>
           </div>
         </div>
-      </div>
-        <div className={styles.line}>
-              <div onMouseEnter={() => {manageMouseEnter()}} onMouseMove={(e) => {manageMouseMove(e)}} onMouseLeave={() => {manageMouseLeave()}} className={styles.box}></div>
-              <svg>
-                <path ref={path}></path>
-              </svg>
-            </div>
-    </section>
+      </section>
     </Element>
   );
 };
---
+++ src/components/Statistics/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Statistics = () => {
+  return (
+    <section>
+      <div className="container">
+        <h1>
+          My <span>Statistics</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Statistics;
